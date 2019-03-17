764a728643d62dfb5ff6518e32e16abc5ec4958d
LorenzoEvans <lorenzo.evans94@gmail.com>
2019-03-16 21:39:30 -0400

    Starting fresh.

--- test/markov_elear/generator_test.clj
+++
@@ -1,9 +0,0 @@
-(ns markov-elear.generator-test
-  (:require [clojure.test :refer :all]
-            [markov-elear.geneator :refer :all]))
-
-(def a-test
-  (testing "FIXME, I don't pass."
-    (is (= 0 1))))
-
-(def list (atom #{}))
