1b232ea2152f4cb025543eae8d454de31c10191b
Lambda_School_Loaner_94 <lambda_school_loaner_94@Lambda-School-Loaner-94s-MacBook-Pro.local>
2019-03-21 15:32:28 -0400

    Added function to generate word chain from word set.

--- markov-elear/src/markov_elear/generator.clj
+++ markov-elear/src/markov_elear/generator.clj
@@ -1,6 +1,27 @@
 (ns markov-elear.generator)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def ex "First Second Third Fourth Fifth Sixth Seventh Eighth Ninth Tenth")
+
+(def words (clojure.string/split ex #" "))
+
+(defn word-transit [] (partition-all 1 2 words))
+(word-transit)
+(merge-with concat {:a [1]} {:a [3]})
+
+(merge-with clojure.set/union {:a #{1}} {:a #{2}})
+
+()
+defn cloj-merge [x y]
+  (merge-with clojure.set/union x y)
+
+
+(defn word-chain [word-transit]
+  (reduce)
+  (fn
+    [r t]
+    (merge-with clojure.set/union r
+                (let [[a b c] t]
+                  {[a b]
+                   (if c #{c} #{})})))
+  {}
+  word-transit)
