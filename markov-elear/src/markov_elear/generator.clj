b36a0b64d09a705c0a607a17da2e096433e5f11f
Lambda_School_Loaner_94 <lambda_school_loaner_94@Lambda-School-Loaner-94s-MacBook-Pro.local>
2019-04-02 16:07:57 -0400

    Adding tests for chain walking/generating text chain.

--- markov-elear/src/markov_elear/generator.clj
+++ markov-elear/src/markov_elear/generator.clj
@@ -3,12 +3,23 @@
 (def ex "And the Golden Grouse and the Pobble who")
 
 (def word-trans
-  (partition-all 3 1(clojure.string/split ex #" ")))
+  (partition-all 3 1 (clojure.string/split ex #" ")))
 
 (defn word-chain [word-trans]
   (reduce (fn [r t]
-              (merge-with clojure.set/union r
-                            (let [[a b c] t]
-                                {[a b] (if c #{} #{})})))
-    {}
-    word-trans))
+            (merge-with clojure.set/union r
+                        (let [[a b c] t]
+                          {[a b] (if c #{} #{})})))
+          {}
+          word-trans))
+
+(defn chain->text [chain]
+  (apply str (interpose " ")))
+
+(defn walk-chain [prefix chain result]
+  (let [suffixes (get chain prefix)]
+    (if (empty? suffixes)
+      result
+      (let [suffix (first (shuffle suffixes))
+            new-prefix [(last prefix) suffix]]
+        (recur new-prefix chain (conj result suffix))))))
